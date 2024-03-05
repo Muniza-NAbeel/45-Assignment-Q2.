702439d6952695f8a470a7f616bf273d8a4e875e
Muniza <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-03-05 14:22:06 +0500

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,3 @@
+// 2 .Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var name1 = "Eric";
+console.log("\"Hello ".concat(name1, " Would you Like to Learn Some Python Today?\""));
